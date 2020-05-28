4efc90848310c4f12295d6a0cccc6b31317b267c
Vinicius Bassalobre <=>
2020-05-28 14:00:02 -0300

    curte todos os posts

--- index.js
+++ index.js
@@ -3,15 +3,18 @@
     require('dotenv').config()
     const facebook = require("./src/facebook")
 
-    await facebook.signup({
-        email: "email@teste.com",
-        firstname: "Jõao",
-        lastname: "da Silva",
-        sex: "m", //m or f 
-        birthday_day: 08, //optional
-        birthday_month: 04,  //optional
-        birthday_year: 1992, //optional
-        password: "S3nh4DoF4cebo0k" //optional
-    })
+    // await facebook.signup({
+    //     email: "email@teste.com",
+    //     firstname: "João",
+    //     lastname: "da Silva",
+    //     sex: "m", //m or f 
+    //     birthday_day: 08, //optional
+    //     birthday_month: 08,  //optional
+    //     birthday_year: 1988, //optional
+    //     password: "S3nh4DoF4cebo0k" //optional
+    // })
+
+    // await facebook.login("user_email", "******")
+    // await facebook.likeAllPosts()
 
 })()
--- src/facebook.js
+++ src/facebook.js
@@ -1,15 +1,16 @@
 const facebook = {
     helper: require("./core/helper"),
     browser: require("./core/browser"),
+    page: undefined,
+    async createPage() {
+        this.page = await this.browser.createPage()
+    },
     async signup(newUser) {
-        const page = await this.browser.createPage()
+        await this.createPage()
 
         await this.helper.describe("acessa o facebook e seta o window.navigator = {} ...", async () => {
-            await page.goto('https:/facebook.com/reg', {
+            await this.page.goto('https:/facebook.com/reg', {
                 waitUntil: 'networkidle2'
-            })
-            await page.evaluate(() => {
-                window.navigator = {}
             })
         })
 
@@ -20,23 +21,26 @@ const facebook = {
         newUser.sex = newUser.sex ? (newUser.sex.toLowerCase() == "f" ? 2 : 1) : this.helper.random("sex")
 
         await this.helper.describe("preenche o formulário de cadastro ...", async () => {
-            await page.waitForSelector("input[name='firstname']")
-            await page.type("input[name='firstname']", newUser.firstname)
-            await page.type("input[name='lastname']", newUser.lastname)
-            await page.type("input[name='reg_email__']", newUser.email)
+            await this.page.waitForSelector("input[name='firstname']")
+            await this.page.type("input[name='firstname']", newUser.firstname)
+            await this.page.type("input[name='lastname']", newUser.lastname)
+            await this.page.type("input[name='reg_email__']", newUser.email)
 
-            await page.waitForSelector("input[name='reg_email_confirmation__']")
-            await page.type("input[name='reg_email_confirmation__']", newUser.email)
-            await page.type("input[name='reg_passwd__']", newUser.password)
+            await this.page.waitForSelector("input[name='reg_email_confirmation__']")
+            await this.page.type("input[name='reg_email_confirmation__']", newUser.email)
+            await this.page.type("input[name='reg_passwd__']", newUser.password)
 
-            await page.select("select[name='birthday_day']", newUser.birthday_day)
-            await page.select("select[name='birthday_month']", newUser.birthday_month)
-            await page.select("select[name='birthday_year']", newUser.birthday_year)
+            await this.page.select("select[name='birthday_day']", newUser.birthday_day)
+            await this.page.select("select[name='birthday_month']", newUser.birthday_month)
+            await this.page.select("select[name='birthday_year']", newUser.birthday_year)
 
-            page.$eval(`input[type='radio'][name='sex'][value='${newUser.sex}']`, elem => elem.click())
-            await page.click("button[name='websubmit']")
+            await this.page.evaluate(({ newUser }) => {
+                document.querySelector(`input[type='radio'][name='sex'][value='${newUser.sex}']`).click()
+            }, { newUser })
 
-            await page.waitFor(parseInt(process.env.WAIT_TIMEOUT))
+            await this.page.click("button[name='websubmit']")
+
+            await this.page.waitFor(parseInt(process.env.WAIT_TIMEOUT))
         })
 
         newUser.sex = newUser.sex == 1 ? "Feminino" : "Masculino"
@@ -44,6 +48,45 @@ const facebook = {
         await this.helper.describe("conta criada ...", async () => {
             console.log(newUser)
             // await this.browser.close()
+        })
+
+    },
+    async login(user, pass) {
+        await this.createPage()
+
+        await this.helper.describe("acessa o facebook ...", async () => {
+            await this.page.goto('https://facebook.com/', {
+                waitUntil: 'networkidle2'
+            })
+        })
+
+        await this.helper.describe("preenche o formulário de login ...", async () => {
+            await this.page.waitForSelector("input[name='email']")
+            await this.page.type("input[name='email']", user)
+            await this.page.type("input[name='pass']", pass)
+            await this.page.type("input[name='pass']", String.fromCharCode(13))
+            await this.page.waitForNavigation()
+        })
+
+        await this.helper.describe("muda pra versao mobile para melhor manipulação ...", async () => {
+            await this.helper.describe("acessa o facebook ...", async () => {
+                await this.page.goto('https://m.facebook.com/', {
+                    waitUntil: 'networkidle2'
+                })
+            })
+        })
+    },
+    async likeAllPosts(limit_scrolls = 5) {
+
+        await this.helper.describe("curte todos os posts visíveis...", async () => {
+            for (let i = 0;i < limit_scrolls;i++) {
+                await this.page.evaluate(() => {
+                    let posts = document.querySelectorAll("article._5rgr._5gh8.async_like ._52jj._15kl._3hwk._4g34 > .touchable:not(._77la)")
+                    posts.forEach(post => post.click())
+                    window.scrollBy(0, document.body.scrollHeight)
+                })
+                await this.page.waitFor(1000)
+            }
         })
 
     }
